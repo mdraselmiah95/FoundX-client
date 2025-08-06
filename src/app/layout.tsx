25738e340f4d633fb411b66f8b2290f699c872f4
mdraselmiah95 <russelroy15@gmail.com>
2025-08-06 22:45:02 +0600

    setup FoundX client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import clsx from "clsx";
 
 import { Providers } from "./providers";
 
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { siteConfig } from "@/src/config/site";
+import { fontSans } from "@/src/config/fonts";
+import { Navbar } from "@/src/components/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -38,16 +38,16 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container flex-grow px-6 pt-16 mx-auto max-w-7xl">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
+            <footer className="flex items-center justify-center w-full py-3">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { siteConfig } from "@/src/config/site";
+import { title, subtitle } from "@/src/components/primitives";
+import { GithubIcon } from "@/src/components/icons";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
+      <div className="justify-center inline-block max-w-xl text-center">
         <span className={title()}>Make&nbsp;</span>
         <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
         <br />
