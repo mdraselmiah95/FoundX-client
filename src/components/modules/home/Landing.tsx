51e025968c488672a06b07de68b1f071a1155b0e
mdraselmiah95 <russelroy15@gmail.com>
2025-08-31 23:27:03 +0600

    setting the layout in the FoundX

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,27 +1,26 @@
-import { Input } from "@heroui/input";
-import React from "react";
+import { Input } from "@nextui-org/input";
 import { SearchIcon } from "../../icons";
 
-const Landing = () => {
+export default function Landing() {
   return (
-    <div className='h-[calc(100vh-64px)] bg-[url("/glass.jpg")] bg-cover bg-center'>
+    <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
       <div className="flex-1 max-w-xl pt-32 mx-auto">
-        <Input
-          aria-label="Search"
-          classNames={{
-            inputWrapper: "bg-default-100",
-            input: "text-sm",
-          }}
-          placeholder="Search..."
-          size="lg"
-          startContent={
-            <SearchIcon className="flex-shrink-0 text-base pointer-events-none text-default-400" />
-          }
-          type="text"
-        />
+        <form className="flex-1">
+          <Input
+            aria-label="Search"
+            classNames={{
+              inputWrapper: "bg-default-100",
+              input: "text-sm",
+            }}
+            placeholder="Search..."
+            size="lg"
+            startContent={
+              <SearchIcon className="flex-shrink-0 text-base pointer-events-none text-default-400" />
+            }
+            type="text"
+          />
+        </form>
       </div>
     </div>
   );
-};
-
-export default Landing;
+}
