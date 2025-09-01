7f770c886519c13a99474fd9d26297af95d238ca
mdraselmiah95 <russelroy15@gmail.com>
2025-09-01 23:25:02 +0600

    new items show

---
+++ src/app/(withCommonLayout)/(home)/RecentPosts.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
+
+import { getRecentPosts } from "@/src/services/RecentPosts";
+
+import { IPost } from "@/src/types";
+import Container from "@/src/components/UI/Container";
+import Card from "@/src/components/UI/Card";
+
+export default async function RecentPosts() {
+  const res = await fetch("=http://localhost:8080/api/v1/items");
+  const data = res.json();
+  console.log({ data });
+  return (
+    <Container>
+      <div className="my-8 section-title">
+        <h2 className="mb-2 text-2xl text-center">Recently Found Items</h2>
+        <p className="text-center">
+          A list of items that have been recently found and reported.
+        </p>
+      </div>
+      <div className="grid justify-center gap-10 my-8 sm:grid-cols-1 md:grid-cols-3">
+        {/* Old Code */}
+        {/* {posts.map((post) => (
+          <p>{post.title}</p>
+        ))} */}
+        {/* {posts.map((post: IPost) => (
+          <Card key={post?._id} post={post} />
+        ))} */}
+      </div>
+      <div className="flex justify-center">
+        <Button className="rounded-md bg-default-900 text-default" size="md">
+          <Link href="/found-items">See All</Link>
+        </Button>
+      </div>
+    </Container>
+  );
+}
